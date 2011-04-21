4fb88cd0382b98da8442bdb94b687b6032441dc1
peo3 <peo314159265@yahoo.co.jp>
2011-04-21 19:23:35 +0900

    Add a simple test script

---
+++ test_all.sh
@@ -0,0 +1,24 @@
+#!/bin/sh
+
+SUBSYS="cpu cpuacct cpuset memory blkio freezer"
+ERRFILE=/tmp/.cgroup-utils.stderr
+
+test_one()
+{
+        $* >/dev/null 2>$ERRFILE
+	ret=$?
+        if [ $ret != 0 -a $ret != 3 ]; then
+                echo "[NG] $*"
+                cat $ERRFILE
+        elif [ $ret = 3 ]; then
+                echo "[??] $*"
+	else
+                echo "[ok] $*"
+	fi
+}
+
+for subsys in $SUBSYS; do
+        test_one cgtree -o $subsys
+        test_one cgshowconfigs -o $subsys
+done
+test_one cgtop -b -n 1
