a85c2c46170d5f4b3988f34c08d1b0c7d2dfda61
peo3 <peo314159265@yahoo.co.jp>
2011-04-22 22:27:00 +0900

    Add a sample script to utilize event_control facility
    linux.c provides eventfd(2) and close(2) syscall wrappers
    to python scripts. cgroup_event_listener script which uses
    the syscalls is a python implementation of cgroup_event_listener.c.
    The script demonstrates event_control facility.

---
+++ cgutils/linux.c
@@ -0,0 +1,78 @@
+/*
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Library General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, USA.
+ *
+ * See the COPYING file for license information.
+ *
+ * Copyright (c) 2011 peo3 <peo314159265@gmail.com>
+ */
+
+/*
+ * Usage in python:
+ *
+ *   import linux
+ *   efd = linux.eventfd(0, 0)
+ *   ...
+ *   ret = struct.unpack('Q', os.read(efd, 8))
+ *   ...
+ *   linux.close(efd)
+ */
+#include <Python.h>
+#include <unistd.h>
+#include <sys/eventfd.h>
+
+static PyObject *
+linux_eventfd(PyObject *self, PyObject *args)
+{
+	int efd, initval, flags;
+
+	if (!PyArg_ParseTuple(args, "ii", &initval, &flags))
+		return NULL;
+	efd = eventfd(initval, flags);
+        if (efd == -1) {
+                return PyErr_SetFromErrno(PyExc_OSError);
+        } else {
+	        return Py_BuildValue("i", efd);
+        }
+}
+
+static PyObject *
+linux_close(PyObject *self, PyObject *args)
+{
+	int ret, fd;
+
+	if (!PyArg_ParseTuple(args, "i", &fd))
+		return NULL;
+	ret = close(fd);
+        if (ret == -1) {
+                return PyErr_SetFromErrno(PyExc_OSError);
+        } else {
+	        return Py_BuildValue("i", ret);
+        }
+}
+
+static PyMethodDef LinuxSyscalls[] = {
+	{"eventfd",  linux_eventfd, METH_VARARGS,
+		"Execute eventfd syscall."},
+	{"close",  linux_close, METH_VARARGS,
+		"Execute close syscall."},
+	{NULL, NULL, 0, NULL}
+};
+
+PyMODINIT_FUNC
+initlinux(void)
+{
+	(void) Py_InitModule("cgutils.linux", LinuxSyscalls);
+}
+
--- setup.py
+++ setup.py
@@ -1,15 +1,20 @@
 #!/usr/bin/python
 
-from distutils.core import setup
+from distutils.core import setup, Extension
 from cgutils.version import VERSION
+
+mod_linux = Extension('linux', sources = ['cgutils/linux.c'])
 
 setup(name = 'cgroup-utils',
       version = VERSION,
       description = 'Utility tools for control groups of Linux',
       long_description =
 '''cgroup-utils includes some useful libraries and tools to view status, statistics and configurations of control groups.''',
-      scripts = ['bin/cgshowconfigs', 'bin/cgtop', 'bin/cgtree'],
+      scripts = ['bin/cgshowconfigs', 'bin/cgtop', 'bin/cgtree',
+                 'bin/cgroup_event_listener'],
       packages = ['cgutils'],
+      ext_package = 'cgutils',
+      ext_modules = [mod_linux],
       author = 'peo3',
       author_email = 'peo314159265@gmail.com',
       url = 'https://github.com/peo3/cgroup-utils',
