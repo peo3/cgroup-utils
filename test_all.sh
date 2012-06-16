d07aecaa84755b27228e52115e94eff9d6da70a0
peo3 <peo314159265@gmail.com>
2012-06-16 22:44:48 +0900

    Improve tests
    We test every stats of cgroups as well as configs of cgroups.
    We choose enabled cgroups by parsing /proc/cgroups to safely
    run tests for a given environment.

--- test_all.sh
+++ test_all.sh
@@ -3,25 +3,37 @@
 SUBSYS="cpu cpuacct cpuset memory blkio freezer net_cls devices"
 ERRFILE=/tmp/.cgroup-utils.stderr
 
+enabled_cgroups=
+while read name _ _ enabled; do
+    if [ $enabled = 1 ]; then
+        if [ $name != perf_event ]; then
+            enabled_cgroups="$enabled_cgroups $name"
+        fi
+    fi
+done < /proc/cgroups
+
 test_one()
 {
-        $* >/dev/null 2>$ERRFILE
+    $* >/dev/null 2>$ERRFILE
 	ret=$?
-        if [ $ret != 0 ]; then
-                grep -q "EnvironmentError: Not enabled in the system" $ERRFILE
-                if [ $? = 0 ]; then
-                        echo "[??] $*"
-                else
-                        echo "[NG] $*"
-                        cat $ERRFILE
-                fi
+    if [ $ret != 0 ]; then
+        grep -q "EnvironmentError: Not enabled in the system" $ERRFILE
+        if [ $? = 0 ]; then
+            echo "[??] $*"
+        else
+            echo "[NG] $*"
+            cat $ERRFILE
+        fi
 	else
-                echo "[ok] $*"
+        echo "[ok] $*"
 	fi
 }
 
-for subsys in $SUBSYS; do
-        test_one cgtree -o $subsys
-        test_one cgshowconfigs -o $subsys
+export PYTHONPATH=.
+
+for subsys in $enabled_cgroups; do
+    test_one python bin/cgtree -o $subsys
+    test_one python bin/cgshowconfigs -o $subsys
+    test_one python bin/cgshowstats -o $subsys
 done
-test_one cgtop -b -n 1
+test_one python bin/cgtop -b -n 1
