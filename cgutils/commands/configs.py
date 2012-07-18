bbb24fa499907c905ce4c4d7fd1106b57f443eaa
peo3 <peo314159265@gmail.com>
2012-07-19 02:03:03 +0900

    Support --show-rate option
    It allows to show a rate to a default or current value
    of a parameter for each cgroup configuration.

--- cgutils/commands/configs.py
+++ cgutils/commands/configs.py
@@ -22,13 +22,29 @@ import optparse
 from cgutils import cgroup
 from cgutils import formatter
 from cgutils.version import VERSION
+from cgutils import host
 
-def print_configs(_cgroup, show_default):
+def calc_memory_rate(val):
+    meminfo = host.MemInfo()
+    meminfo.update()
+    return float(val) / meminfo['MemTotal']
+
+_support_rate = {
+    'limit_in_bytes': calc_memory_rate,
+    'soft_limit_in_bytes': calc_memory_rate,
+    'memsw.limit_in_bytes': calc_memory_rate,
+    'kmem.tcp.limit_in_bytes': calc_memory_rate,
+    'swappiness': None,
+    'shares': None,
+    'weight': None,
+    }
+
+def print_configs(_cgroup, options):
     configs = _cgroup.get_configs()
     defaults = _cgroup.get_default_configs()
     cg_shown = False
     for name, val in configs.iteritems():
-        if not show_default and defaults[name] == val:
+        if not options.show_default and defaults[name] == val:
             continue
         if 'in_bytes' in name:
             if val == defaults[name]:
@@ -37,10 +53,20 @@ def print_configs(_cgroup, show_default):
                 valstr = formatter.byte2str(val)
         else:
             valstr = str(val)
+        if options.show_rate and name in _support_rate:
+            if _support_rate[name]:
+                rate = _support_rate[name](val)
+            else:
+                rate = float(val) / defaults[name]
+            ratestr = ' (%s)' % formatter.percent2str(rate)
+        else:
+            ratestr = ''
+
         if not cg_shown:
+            # Want to show only when at least one of configs is changed
             print(_cgroup.fullname)
             cg_shown = True
-        print("\t%s=%s" % (name, valstr))
+        print("\t%s=%s%s" % (name, valstr, ratestr))
 
 def run(args, options):
     root_cgroup = cgroup.scan_cgroups(options.target_subsystem)
@@ -52,7 +78,7 @@ def run(args, options):
         if options.hide_empty and _cgroup.n_procs == 0:
             pass
         else:
-            print_configs(_cgroup, options.show_default)
+            print_configs(_cgroup, options)
         for child in _cgroup.childs:
             print_cgroups_recursively(child)
     print_cgroups_recursively(root_cgroup)
@@ -66,6 +92,9 @@ parser.add_option('-o', action='store', type='string',
 parser.add_option('--show-default', action='store_true',
                   dest='show_default', default=False,
                   help='Show every parameters including default values [False]')
+parser.add_option('--show-rate', action='store_true',
+                  dest='show_rate', default=False,
+                  help='Show rate value to default/current values [False]')
 parser.add_option('-e', '--hide-empty', action='store_true',
                   dest='hide_empty', default=False,
                   help='Hide empty groups [False]')
